15693bb6f8de1667185e39262e024e84c9d77011
ajohnson132490 <44075033+ajohnson132490@users.noreply.github.com>
2021-12-02 20:51:19 -0500

    initial

---
+++ classes.py
@@ -0,0 +1,8 @@
+class item:
+    def __init__(self, name, buyPrice, buyDate):
+        self.name = name
+        self.buyPrice = buyPrice
+        self.buyDate = buyDate
+
+    def getName():
+        return self.name
